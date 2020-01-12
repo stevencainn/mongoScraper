933638a604a92ac5b319ad7a858a82b17ba5fb77
steven cain <stevencainn@gmail.com>
2020-01-12 16:41:57 -0500

    did controllers

---
+++ controllers/controller.js
@@ -0,0 +1,103 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+
+// *** Scraping Tools *** //
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("../models");
+
+// *** Route for Scraping *** //
+router.get("/scrape", function (req, res) {
+    // Get the entire body of the html with a request.
+    axios.get("https://www.theonion.com/c/news-in-brief")
+        .then(function (response) {
+            // Load the response into cheerio and save it as a short-hand selector "$"
+            var $ = cheerio.load(response.data);
+
+            // Get every h1 within an article tag...
+            $("article").each(function (i, element) {
+                // Save an empty result object
+                var result = {};
+                console.log(result);
+                // Get the text and href of every link, save them as properties of the result object.
+                result.title = $(this).find("h1.headline").text();
+                result.link = $(this).find("h1.headline").children("a").attr("href");
+                result.summary = $(this).find("div.excerpt").children("p").text();
+                result.image = $(this).find("picture").children("source").attr("data-srcset");
+
+                // Create a new Article with the `result` object built from scraping.
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        // View the added result in the console:
+                        // console.log(dbArticle);
+                    })
+                    .catch(function (error) {
+                        // Send the error, if it exists.
+                        return res.json(error);
+                    });
+            });
+
+            // Alert the client if the scrape was completed:
+            res.send("Scrape was successful!");
+        });
+});
+
+// *** Routes to export to server.js *** //
+
+// Route to get all Articles from the db.
+router.get("/", function (req, res) {
+    // Limit set to only show first 20 articles.
+    db.Article.find({}).limit(20)
+        .then(function (scrapedData) {
+            // Save all scraped data into a handlebars object.
+            var hbsObject = { articles: scrapedData };
+            console.log(hbsObject);
+            // Send all found articles as an object to be used in the handlebars receieving section of the index.
+            res.render("index", hbsObject);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+// Route to save an Article.
+router.put("/saved/:id", function (req, res) {
+    // Update the article's boolean "saved" status to 'true.'
+    db.Article.update(
+        { _id: req.params.id },
+        { saved: true }
+    )
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+// Route to drop the Articles collection.
+router.delete("/drop-articles", function (req, res, next) {
+    db.Article.remove({}, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("articles dropped!");
+        }
+    })
+        .then(function (dropnotes) {
+            db.Note.remove({}, function (err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("notes dropped!");
+                }
+            })
+        })
+});
+
+module.exports = router;
---
+++ controllers/saved-articles.js
@@ -0,0 +1,124 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+
+// Require all models
+var db = require("../models");
+
+// Route to get all saved Articles from the db.
+router.get("/saved-articles", function (req, res) {
+
+    db.Article.find({})
+        .then(function (savedData) {
+            // Save all saved article data into a handlebars object.
+            var hbsObject = { articles: savedData };
+            // console.log(hbsObject);
+            // Send all found saved articles as an object to be used in the handlebars receieving section of the index.
+            res.render("saved", hbsObject);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+// Route to get a specific saved Article and populate it with its notes.
+router.get("/getnotes/:id", function (req, res) {
+    // Find the article by req.params.id,
+    db.Article.findOne(
+        { _id: req.params.id }
+    )
+        // run the populate method with note,
+        .populate("notes")
+        // respond with the article with the note included.
+        .then(function (dbArticle) {
+            // If all Articles are successfully found, send them back to the client.
+            res.json(dbArticle);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+// Route for saving/updating an Article's associated Note.
+router.post("/postnotes/:id", function (req, res) {
+    // Save the new note that gets posted to the Notes collection,
+    // then find an article from the req.params.id,
+    // and update it's "note" property with the _id of the new note.
+    console.log(req.body);
+    console.log("xxxxxxx");
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate(
+                { _id: req.params.id },
+                {
+                    $push:
+                        { notes: dbNote._id }
+                },
+                // {note: dbNote._id},
+                { new: true }
+            );
+        })
+        // respond with the article with the note included.
+        .then(function (dbArticle) {
+            // If all Notes are successfully found, send them back to the client.
+            res.json(dbArticle);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+// Route for updating a Note.
+router.get("/getsinglenote/:id", function (req, res) {
+    db.Note.findOne(
+        { _id: req.params.id }
+    )
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (error) {
+            res.json(error);
+        });
+});
+
+// Route to delete a Note.
+router.delete("/deletenote/:id", function (req, res) {
+    db.Note.remove(
+        { _id: req.params.id }
+    )
+        // Todo - remove note from article as well
+        // .then(function (dbArticle) {
+        //     db.Article.findOneAndDelete(
+        //         {_id: req.params.id},
+        //         {notes: dbNote._id},
+        //         // {new: true }
+        //     );
+        // })
+        .then(function (dbNote) {
+            res.json(dbNote);
+        })
+        .catch(function (error) {
+            res.json(error);
+        });
+});
+
+// Route to return (unsave) an Article.
+router.put("/returned/:id", function (req, res) {
+    // Update the article's boolean "saved" status to 'false.'
+    db.Article.update(
+        { _id: req.params.id },
+        { saved: false }
+    )
+        .then(function (result) {
+            res.json(result);
+        })
+        .catch(function (error) {
+            // If an error occurs, send the error to the client.
+            res.json(error);
+        });
+});
+
+module.exports = router;
