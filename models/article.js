d08bfb6be0fdd3080a889c4c0bbb7c3890a5c5db
steven cain <stevencainn@gmail.com>
2020-01-12 16:44:20 -0500

    did models folders

--- models/article.js
+++ models/article.js
@@ -4,29 +4,40 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
-  title: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
-  },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  comment: {
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    // `saved` is a boolean used to display the article in the raw, scraped section, or the saved articles.
+    saved: {
+        type: Boolean,
+        default: false
+    },
+    // `note` is an object that stores a Note id
+    // The ref property links the ObjectId to the Note model
+    // This allows us to populate the Article with an associated Note
+    notes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    ]
 });
 
-// This creates our model from the above schema, using mongoose's model method
+// Creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
--- models/comment.js
+++ models/comment.js
@@ -4,16 +4,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
-var CommentSchema = new Schema({
-  // `title` is of type String
+var NoteSchema = new Schema({
   title: String,
-  // `body` is of type String
   body: String
 });
 
-// This creates our model from the above schema, using mongoose's model method
-var Comment = mongoose.model("Comment", CommentSchema);
+// Creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", NoteSchema);
 
 // Export the Note model
-module.exports = Comment;
+module.exports = Note;
